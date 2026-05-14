56a6028ee65cde6356b125a145ba6f5b9513cbab
gilferreira08 <gilferreira08@yahoo.com.br>
2026-05-14 12:09:15 +0200

    Update knowledge.py

--- knowledge.py
+++ knowledge.py
@@ -1,1 +1,30 @@
+TARGET_GEOS = [
+    "France",
+    "Portugal",
+    "Switzerland",
+    "Luxembourg",
+    "Brazil",
+    "Remote Europe",
+]
 
+EXCLUDED_KEYWORDS = [
+    "accounting",
+    "audit",
+    "ap",
+    "ar",
+    "intern",
+    "graduate",
+    "tax",
+    "back office",
+    "compliance",
+]
+
+BOARD_MEMBERS = [
+    "HR Director",
+    "CFO",
+    "Head of Treasury",
+    "Hiring Manager",
+    "FP&A Manager",
+    "Financial Risk Manager",
+    "Project Finance Director",
+]
